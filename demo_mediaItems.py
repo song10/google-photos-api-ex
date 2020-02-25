0faa55f57a8f6ac7159cf543e6f006412e33fcf4
Rex Zhuo <rex.zhuo@outlook.com>
2020-02-25 16:06:56 +0800

    extend mediaItems demo (part 4)

--- demo_mediaItems.py
+++ demo_mediaItems.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from init_photo_service import service
 import pandas as pd
 
@@ -198,3 +197,77 @@ request_body = {
     }
 }
 df_search_result = pd.DataFrame(response_media_items_by_filter(request_body))
+
+
+"""
+batchCreate method
+"""
+import os
+import pickle
+import requests
+
+# step 1: Upload byte data to Google Server
+image_dir = os.path.join(os.getcwd(), 'Images To Upload')
+upload_url = 'https://photoslibrary.googleapis.com/v1/uploads'
+token = pickle.load(open('token_photoslibrary_v1.pickle', 'rb'))
+
+headers = {
+    'Authorization': 'Bearer ' + token.token,
+    'Content-type': 'application/octet-stream',
+    'X-Goog-Upload-Protocol': 'raw'
+}
+
+image_file = os.path.join(image_dir, 'dogy.jpg')
+headers['X-Goog-Upload-File-Name'] = 'dogy_The_Corgi.jpg'
+
+img = open(image_file, 'rb').read()
+response = requests.post(upload_url, data=img, headers=headers)
+
+request_body = {
+    'newMediaItems': [
+        {
+            'description': 'dogy the corgi',
+            'simpleMediaItem': {
+                'uploadToken': response.content.decode('utf-8')
+            }
+        }
+    ]
+}
+
+upload_response = service.mediaItems().batchCreate(body=request_body).execute()
+
+
+def upload_image(image_path, upload_file_name, token):
+    headers = {
+        'Authorization': 'Bearer ' + token.token,
+        'Content-type': 'application/octet-stream',
+        'X-Goog-Upload-Protocol': 'raw',
+        'X-Goog-Upload-File-Name': upload_file_name
+    }
+
+    img = open(image_path, 'rb').read()
+    response = requests.post(upload_url, data=img, headers=headers)
+    print('\nUpload token: {0}'.format(response.content.decode('utf-8')))
+    return response
+
+
+tokens = []
+image_dir = os.path.join(os.getcwd(), 'Images To Upload')
+upload_url = 'https://photoslibrary.googleapis.com/v1/uploads'
+token = pickle.load(open('token_photoslibrary_v1.pickle', 'rb'))
+
+image_skytower = os.path.join(image_dir, 'rabbit.jpg')
+response = upload_image(image_skytower, 'Cute Rabbit.jpg', token)
+tokens.append(response.content.decode('utf-8'))
+
+image_sunset = os.path.join(image_dir, 'seal.jpg')
+response = upload_image(image_sunset, os.path.basename(image_sunset), token)
+tokens.append(response.content.decode('utf-8'))
+
+new_media_items = [{'simpleMediaItem': {'uploadToken': tok}}for tok in tokens]
+
+request_body = {
+    'newMediaItems': new_media_items
+}
+
+upload_response = service.mediaItems().batchCreate(body=request_body).execute()
