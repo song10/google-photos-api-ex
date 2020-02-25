b39e0d62890c7eb1cbdee197d5754e21a0ea0bb7
Rex Zhuo <rex.zhuo@outlook.com>
2020-02-25 16:51:00 +0800

    extend albums demo (part 5)

--- demo_albums.py
+++ demo_albums.py
@@ -138,3 +138,59 @@ response = service.albums().share(
 
 
 service.albums().unshare(albumId=response_album_family_photos.get('id')).execute()
+
+"""
+batchAddMediaItems & batchRemoveMediaItems
+"""
+response = service.albums().list(
+    pageSize=50,
+    excludeNonAppCreatedData=False
+).execute()
+
+lstAlbums = response.get('albums')
+nextPageToken = response.get('nextPageToken')
+
+while nextPageToken:
+    response = service.albums.list(
+        pageSize=50,
+        excludeNonAppCreatedData=False,
+        pageToken=nextPageToken
+    )
+    lstAlbums.extend(response.get('ablums'))
+    nextPageToken = response.get('nextPageToken')
+
+df_albums = pd.DataFrame(lstAlbums)
+
+album_id = df_albums['id'][0] # 'My Family Photos'
+
+
+response = service.mediaItems().list(pageSize=25).execute()
+
+lst_medias = response.get('mediaItems')
+nextPageToken = response.get('nextPageToken')
+
+while nextPageToken:
+    response = service.mediaItems().list(
+        pageSize=25,
+        pageToken=nextPageToken
+    ).execute()
+    lst_medias.extend(response.get('mediaItems'))
+    nextPageToken = response.get('nextPageToken')
+
+df_media_items = pd.DataFrame(lst_medias)
+media_items_ids = df_media_items['id'][:3].to_list()
+
+request_body = {
+    'mediaItemIds': media_items_ids
+}
+
+response = service.albums().batchAddMediaItems(
+    albumId=album_id,
+    body=request_body
+).execute()
+
+
+response = service.albums().batchRemoveMediaItems(
+    albumId=album_id,
+    body=request_body
+).execute()
---
+++ pd_settings.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.max_colwidth', 32)
+pd.set_option('display.width', 120)
+pd.set_option('expand_frame_repr', True)
